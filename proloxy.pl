76062a9ec03ed89a26e25d9d405e5f1b4405e96d
Markus Triska <triska@metalevel.at>
2015-11-16 20:10:58 +0100

    add documentation, MIT license

--- proloxy.pl
+++ proloxy.pl
@@ -1,23 +1,38 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    HTTP reverse proxy.
 
-   Currently very preliminary:
+   This lets you relay traffic to different web servers, reachable via
+   dedicated paths and a common umbrella URL.
 
-   It only directs <File> to localhost:4041/<File>, that is, it just
-   directs everything to a different port.
+   Proloxy needs SWI-Prolog >= 7.3.12.
 
-   Eventually, we want to relay requests to different ports based on
-   flexible rules.
+   Copyright (c) 2015 Markus Triska (triska@metalevel.at)
 
-   This requires SWI-Prolog >= 7.3.12.
+   Permission is hereby granted, free of charge, to any person
+   obtaining a copy of this software and associated documentation
+   files (the "Software"), to deal in the Software without
+   restriction, including without limitation the rights to use, copy,
+   modify, merge, publish, distribute, sublicense, and/or sell copies
+   of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
 
-   Written by Markus Triska, July 2015.
-   Public domain code.
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 :- module(proloxy,
-	  [ proloxy/1			% +Port
-	  ]).
+          [ proloxy/1,                  % +Port
+            proloxy_https/1             % +Port
+          ]).
 
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
@@ -30,7 +45,33 @@
 :- use_module(library(http/html_write)).
 :- use_module(library(http/http_server_files)).
 
-%:- initialization http_daemon.
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   Configuration
+   =============
+
+   Different web services are reachable via different paths.
+
+   For example, in the situation below we relay requests for:
+
+      /rits/<File>
+
+   to http://localhost:4043/<File>, where a RITS HTTP server handles
+   the request. Note that '/rits' needs to be specified as the prefix
+   in handle_request/2 too, because redirects that the RITS server
+   emits need to be rewritten: If the RITS server redirects to <R>,
+   then the proxy needs to redirect the client to /rits/<R>, so that
+   the new request is again delegated to the RITS server.
+
+   In the example below, all other requests are relayed to a local web
+   server on port 4041, passing the original request path unmodified.
+   This different server can for example host the site's main page.
+
+   You can add new services below, using a dedicated prefix for each.
+
+   An additional feature: You can easily enable HTTPS for all web
+   servers at once by making Proloxy itself use HTTPS for the client
+   connection (the Proloxy<->servers connections stay local).
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 :- http_handler('/test',
                 handle_request('/test', 'http://localhost:4041'),
@@ -40,6 +81,7 @@
                 handle_request('/rits', 'http://localhost:4043'),
                 [prefix]).
 
+%:- initialization http_daemon.
 
 :- debug(proloxy).
 
@@ -57,9 +99,9 @@ handle_request(Prefix, To, Request) :-
         atomic_list_concat([To,URI], Target),
         debug(proloxy, "target: ~q\n", [Target]),
         method_pure(Method0, Method),
-        proxy(Method, Prefix, Target, Request).
+        proxy(Method, Prefix, URI0, Target, Request).
 
-proxy(data(Method), _, Target, Request) :-
+proxy(data(Method), _, _, Target, Request) :-
         read_data(Request, Data),
         http_open(Target, In, [method(Method), post(Data),
                                % cert_verify_hook(cert_accept_any),
@@ -67,7 +109,7 @@ proxy(data(Method), _, Target, Request) :-
         call_cleanup(read_string(In, _, Bytes),
                      close(In)),
         throw(http_reply(bytes(ContentType, Bytes))).
-proxy(other(Method), Prefix, Target, _) :-
+proxy(other(Method), Prefix, URI, Target, _) :-
         http_open(Target, In, [method(Method),
                                % cert_verify_hook(cert_accept_any),
                                redirect(false),
@@ -83,6 +125,8 @@ proxy(other(Method), Prefix, Target, _) :-
             ),
             Reply =.. [Status,Location],
             throw(http_reply(Reply))
+        ;   Code == 404 ->
+            throw(http_reply(not_found(URI)))
         ;   throw(http_reply(bytes(ContentType, Bytes)))
         ).
 
@@ -107,7 +151,13 @@ defaulty_pure(M, other(M)).
 proloxy(Port) :-
 	http_server(http_dispatch, [port(Port)]).
 
-https_proloxy(Port) :-
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   proloxy_https/1 is like proloxy/1, but uses HTTPS.
+
+   You need a certificate and key to make this work.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+proloxy_https(Port) :-
 	http_server(http_dispatch,
 		    [ port(Port),
 		      ssl([ certificate_file('server.crt'),
